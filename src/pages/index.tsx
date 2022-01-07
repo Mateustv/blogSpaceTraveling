65015f06d01205cba860c95be51b14eda78148a9
Mateus T <mateustavarestv@hotmail.com>
2022-01-06 23:12:32 -0300

    fazendo por conta propria uma requicisão no prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client'
+import { FiCalendar, FiUser } from "react-icons/fi";
+import { format } from 'date-fns'
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +27,55 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <section className={styles.contentBlog}>
+          <h1>Como ultilizar Hooks</h1>
+          <p>pensando em sincronização em vez  de ciclos de vidas</p>
+          <span><FiCalendar />  {format(new Date(), 'dd MMM yyyy')}</span>
+          <span><FiUser /> Mateus Tavares </span>
+        </section>
+      </main>
+    </>
+  )
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
 
-//   // TODO
-// };
+  const prismic = getPrismicClient();
+
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')], {
+    fetch: ["post.title", 'post.subtitle', 'post.author'],
+    pageSize: 1,
+  }
+  );
+
+  // console.log(JSON.stringify(postsResponse, null, 2))
+
+  const results = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: format(new Date(post.last_publication_date), 'dd MMM yyyy'),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author
+      }
+    }
+  })
+
+  const next_page = postsResponse.next_page
+
+  return {
+    props: {
+      postsPagination: {
+        next_page,
+        results,
+      },
+    }
+  }
+
+};
